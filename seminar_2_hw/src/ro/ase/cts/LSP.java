35d967e73fc01f2e637f473a2208ce1366ec2823
Copaci Maria <117281714+copacimaria@users.noreply.github.com>
2024-03-01 00:41:04 +0200

    Add files via upload

---
+++ seminar_2_hw/src/ro/ase/cts/LSP.java
@@ -0,0 +1,35 @@
+package ro.ase.cts;
+
+
+//Liskov Substitution Principle (LSP)
+interface Heigth{
+    void setHeight();
+}
+interface Width{
+    void setWidth();
+}
+
+class Square implements Width,Heigth{
+    @Override
+    public void setHeight() {
+
+    }
+
+    @Override
+    public void setWidth() {
+
+    }
+}
+class Circle implements Width,Heigth{
+
+    @Override
+    public void setHeight() {
+
+    }
+
+    @Override
+    public void setWidth() {
+
+    }
+}
+
---
+++ seminar_2_hw/src/ro/ase/cts/SRP.java
@@ -0,0 +1,14 @@
+package ro.ase.cts;
+
+//Single Responsibility Principle (SRP)
+class VehicleDetails{
+    public void printDetails(){}
+}
+
+class VehicleCalculateValues{
+    public void calculateValues(){}
+}
+
+class VehicleAddToDB{
+    public void addVehicleToDB(){}
+}
